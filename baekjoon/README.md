6e6a81e6dab40556c33ebbf37d5ba4c4c8fe4d91
ant <rkdwldms42@naver.com>
2023-11-08 14:14:15 +0900

    pull

--- baekjoon/README.md
+++ baekjoon/README.md
@@ -1,1 +1,2 @@
-An absolute bare-bones web app.
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ baekjoon/test/baekjoon_1_test.dart
@@ -0,0 +1,8 @@
+import 'package:baekjoon_1/baekjoon_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
